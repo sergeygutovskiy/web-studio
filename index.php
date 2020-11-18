351d4dc9c3d5e9070f085053561fa1ff0007bd4c
Kirill228sexy <kirillteterehov@yandex.ru->
2020-11-18 17:37:51 +0300

    new menu

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-	<title>Студия </title>
-	<meta content="width=device-width, initial-scale=1" name="viewport" />
 
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+<title>Digital-агенство terexov</title>
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-	<link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet">
-	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<link rel="stylesheet" href="fonts/font-awesome/css/all.css">
+<link rel="stylesheet" type="text/css" href="style/main.css">
+<link rel="stylesheet" type="text/css" href="style/index.css">
+<link rel="stylesheet" type="text/css" href="style/_navigation.css">
 
-	<link rel="stylesheet" href="../fonts/font-awesome/css/all.css">
-
-	<link rel="stylesheet" type="text/css" href="style/main.css">
-	<link rel="stylesheet" type="text/css" href="style/index.css">
-	<link rel="stylesheet" type="text/css" href="style/_navigation.css">
-
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 </head>
 
 <body>
-	<?php require "templates/_navigation.php"; ?>
- <header >
-        
-</header>
+<?php require "templates/_navigation.php"; ?>
+<?php require "templates/_coursor.php"; ?>
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+
+
+
 </body>
 </html>
---
+++ templates/_coursor.php
@@ -0,0 +1,151 @@
+<!-- Custom-cursor -->
+<div class="ppk-dot-outline"></div>
+<div class="ppk-dot"></div>
+<style>
+    
+    *{
+        cursor:none;
+    }
+  /* Custom Cursor */
+  .ppk-dot,
+  .ppk-dot-outline {
+	z-index: 999;
+	pointer-events: none;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	border-radius: 50%;
+	opacity: 0;
+	-webkit-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+	transition: opacity 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+	transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+	transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+  }
+  
+  .ppk-dot {
+	width: 8px;
+	height: 8px;
+	background-color: #feac43;
+  }
+  
+  .ppk-dot-outline {
+	width: 40px;
+	height: 40px;
+	background-color: rgba(254, 172, 67, 0.5);
+  }
+  
+</style>
+<script>
+	
+    var cursor = {
+        delay: 8,
+        _x: 0,
+        _y: 0,
+        endX: (window.innerWidth / 2),
+        endY: (window.innerHeight / 2),
+        cursorVisible: true,
+        cursorEnlarged: false,
+        $dot: document.querySelector('.ppk-dot'),
+        $outline: document.querySelector('.ppk-dot-outline'),
+        
+        init: function() {
+            // Set up element sizes
+            this.dotSize = this.$dot.offsetWidth;
+            this.outlineSize = this.$outline.offsetWidth;
+            
+            this.setupEventListeners();
+            this.animateDotOutline();
+        },
+        
+        setupEventListeners: function() {
+            var self = this;
+            
+            // Anchor hovering
+            document.querySelectorAll('a').forEach(function(el) {
+                el.addEventListener('mouseover', function() {
+                    self.cursorEnlarged = true;
+                    self.toggleCursorSize();
+                });
+                el.addEventListener('mouseout', function() {
+                    self.cursorEnlarged = false;
+                    self.toggleCursorSize();
+                });
+            });
+            
+            // Click events
+            document.addEventListener('mousedown', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            document.addEventListener('mouseup', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+      
+            document.addEventListener('mousemove', function(e) {
+                // Show the cursor
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+    
+                // Position the dot
+                self.endX = e.pageX;
+                self.endY = e.pageY;
+                self.$dot.style.top = self.endY + 'px';
+                self.$dot.style.left = self.endX + 'px';
+            });
+            
+            // Hide/show cursor
+            document.addEventListener('mouseenter', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            });
+            
+            document.addEventListener('mouseleave', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            });
+        },
+        
+        animateDotOutline: function() {
+            var self = this;
+            
+            self._x += (self.endX - self._x) / self.delay;
+            self._y += (self.endY - self._y) / self.delay;
+            self.$outline.style.top = self._y + 'px';
+            self.$outline.style.left = self._x + 'px';
+            
+            requestAnimationFrame(this.animateDotOutline.bind(self));
+        },
+        
+        toggleCursorSize: function() {
+            var self = this;
+            
+            if (self.cursorEnlarged) {
+                self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            } else {
+                self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+            }
+        },
+        
+        toggleCursorVisibility: function() {
+            var self = this;
+            
+            if (self.cursorVisible) {
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            } else {
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            }
+        }
+    }
+    
+    cursor.init();
+</script>
