118890ace80b29946f164ed4c0b2053326004108
Kirill228sexy <kirillteterehov@yandex.ru->
2020-12-24 17:50:41 +0300

    cdek

---
+++ projects/cdek.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Oswald&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/fonts/font-awesome/css/all.css">
+  <link rel="stylesheet" href="/style/index.css">
+  <link rel="stylesheet" href="/style/_navigation.css">
+  <link rel="stylesheet" href="/style/slick.css">
+  <link rel="stylesheet" href="/style/info.css">
+  <link rel="stylesheet" href="/style/_footer.css">
+  <link rel="stylesheet" href="/style/general.css">
+  <link rel="stylesheet" href="/style/main.css">
+  
+  
+</head>
+<body>
+  
+<?php require "../templates/_navigation.php"; ?>
+  
+   <section class="info">
+       <div class="container">
+           <div class="info__inner">
+               <div class="general__top">
+               <div class="info__title">Сдек</div>
+               <div class="info__subtitle">Website + Design + SEO</div>
+               <a href="https://logist-cdek.ru" class="info__link">logist-cdek.ru</a>
+               </div>
+           </div>
+       </div>
+       
+       
+
+       <div class="container">
+       <div class="container-fluid">
+           <div class="info__banner">
+               <img src="\images\content\5fc107ffcb7bc.jpg" alt="">
+           </div>
+       </div>
+
+
+           <div class="infotask">
+           
+            <div class="infotask__title">Главная</div>
+            <div class="infotask__text">Все элементы дизайна были нарисованы от руки, учитывая коропративные шрифты компании, удобное расположение всех элементов помогает пользователю хорошо ориентироваться на сайте</div>
+           </div>
+            
+           <div class="container-fluid">
+           <div class="info__banner">
+               <img src="\images\content\5fc107ffcb7bc.jpg" alt="">
+           </div>
+            </div>
+
+       <div class="infotask">
+           
+            <div class="infotask__title">Калькулятор</div>
+            <div class="infotask__text">Создали калькулятор, который помогает пользователям сразу оценивать стоимость доставки в реальном времени благодаря нескольким параметрам</div>
+           </div>
+           
+           <div class="container-fluid">
+           <div class="info__banner">
+               <img src="\images\content\5fc107ffcb7bc.jpg" alt="">
+           </div>
+            </div>
+
+           <div class="info__links">
+            <a class="goback" href="#">Ко всем проектам</a>
+            
+            </div>
+       </div>
+   </section>
+
+   <?php require "../templates/_footer.php"; ?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="/js/slick.min.js"></script>
+  <script src="https://use.fontawesome.com/1295a37c31.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="/js/main.js"></script>
+</body>
+</html>
--- templates/_navigation.php
+++ templates/_navigation.php
@@ -14,10 +14,10 @@
 
 		<div class="nav__links-container">
 			<ul class="nav__links">
-				<li class="nav__link"><a href="projects.php"> Работы  </a></li>
-				<li class="nav__link"><a href="about.php"> О нас    </a></li>
-				<li class="nav__link"><a href="services.php"> Услуги   </a></li>
-				<li class="nav__link"><a href="contacts.php"> Контакты </a></li>
+				<li class="nav__link"><a href="/projects.php"> Работы  </a></li>
+				<li class="nav__link"><a href="/about.php"> О нас    </a></li>
+				<li class="nav__link"><a href="/services.php"> Услуги   </a></li>
+				<li class="nav__link"><a href="/contacts.php"> Контакты </a></li>
 				<li class="nav__link nav__phone"><a href="tel:+7 952 246-76-79">+7 952 246-76-79</a></li>
 			</ul>
 		</div>
