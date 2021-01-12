3ecc331f29c219137cb45cd1d940221493ab976a
Kirill228sexy <kirillteterehov@yandex.ru->
2021-01-12 20:55:44 +0300

    Update mail.php

--- templates/mail.php
+++ templates/mail.php
@@ -1,12 +1,14 @@
  <?php
 
 $recepient = "kirillteterehov@yandex.ru";
-$sitename = "ЭраСтройМонтаж";
+$sitename = "Сайт Студии";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 
 $message = "Имя: $name \nТелефон: $phone";
+
+var_dump($phone, $name);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
